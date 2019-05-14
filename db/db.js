288f588635d36acb67d6c80289e9d5d7190be1e8
Matthew Gonczar <mattisagumball@gmail.com>
2019-05-14 15:55:02 -0500

    fix db bug

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+const connectionString = process.env.MONGODB_URI;
+
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
